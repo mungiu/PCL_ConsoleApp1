0f0a1eefef5a3c1b71548f67f0a7fdecd80c028b
Andrei Mungiu <andrei_mungiu@yahoo.com>
2021-02-25 10:42:24 +0300

    HW WIP

--- Exercise2c.fs
+++ Exercise2c.fs
@@ -20,4 +20,24 @@ let rec mySumFold foldFn accumulator iterable =
         accumulator //if empty return accumulator
     | head::tail ->
         foldFn head :: mySumFold foldFn [] tail
-myFold addNum10 [] [1; 2; 3] 
+mySumFold addNum10 [] [1; 2; 3]
+
+
+
+let rec myFoldBack foldFn accumulator iterable =
+    match iterable with
+    | [] -> 
+        accumulator //if empty return accumulator
+    | head::tail ->
+        accumulator + foldFn (myFoldBack foldFn 0 tail) head 
+myFoldBack (+) 0 [1; 2; 3]
+
+
+
+// summing up all elements in a list using myFoldBack
+let mySumList lst =
+    match lst with
+    | [] -> 0 // if the list is empty
+    // :: and [] are called constructors, they construct data structures
+    | (hd::rest) -> myFoldBack (+) hd rest //if the list containst head and tail (cnotains at least one element)
+mySumList [2; 3; 5]
