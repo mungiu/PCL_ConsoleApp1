3d86f1b145ba60532484477d3fc858ec8b9da377
Andrei Mungiu <andreimungiu@hotmail.com>
2021-03-11 12:11:05 +0100

    Lesson 5 finalized

---
+++ Exercise 5.fs
@@ -0,0 +1,3 @@
+﻿module Exercise_5
+
+// Redefine the pclArea function (defined in Exercise 6.1) to use the new data type (PclShapeR). Call the new function pclAreaR
---
+++ Lesson 5.fs
@@ -0,0 +1,98 @@
+﻿module Lesson_5
+
+let rec sumList lst =
+    match lst with
+    [] -> 0
+    | hd :: tl -> hd + sumList tl
+sumList [1 .. 1000000]    // this generates a stack overflow
+
+let rec i_am_tail_recursive arg = 
+    // for completeness check for bound
+    printf "%d" arg // here arg will have a new value inside each recursive call
+    if (arg = 1000) then true
+    else i_am_tail_recursive (arg + 1)
+i_am_tail_recursive(1000)
+
+// recursiveversion without StackOverflow
+// the last thing done in a function must the the recursive call itself
+// nothing else must be done after that to avoid StackOverflow
+let sumListRefactored lst =
+    let rec sumListAcumTR lst accumulator =
+        match lst with
+        [] -> accumulator
+        | hd :: tl -> sumListAcumTR tl (accumulator + hd)
+    sumListAcumTR lst 0
+sumListRefactored [1 .. 1000000]
+
+//print a list of integers in reverse using continuation
+// continuation allows doing things intermediatelly, not jsut accumulating
+let printInReverse lst =
+    let rec printInReverseTR lst cont =
+        match lst with
+        // executing then continuation function
+        [] -> cont()
+        // for other lists, add printing of the current
+        // element as part of the continuation
+        | hd :: tl -> printInReverseTR tl (fun() -> printf "%d" hd
+                                                    cont())
+    printInReverseTR lst (fun() -> printfn "DONE")
+[0 .. 10] |> printInReverse
+
+//print a list of integers in reverse using the accumulator
+let printInReverseRefactored lst =
+    let rec printInReverseTR lst accum =
+        match lst with
+        // executing then continuation function
+        [] -> accum
+        // for other lists, add printing of the current
+        // element as part of the continuation
+        | hd :: tl -> printInReverseTR tl (hd :: accum)
+    printInReverseTR lst []
+[0 .. 10] |> printInReverseRefactored
+
+
+let rec factorial x =
+    if x <= 1 then 1 //base case
+    else x * factorial (x-1)
+
+// HERE THE CONCEPT IS OK BUT THE CODE IS WRONG
+let accFactorial x =
+    let rec tailRecFactorialTR x acc =
+        if x <= 1 
+        then acc
+        else tailRecFactorialTR (x - 1) (acc * x)
+    tailRecFactorialTR x 1
+accFactorial 10
+
+// 
+let accFactorialWithContinuation x =
+    let rec tailRecFactorialTR x cont =
+        if x <= 1 
+        then cont 1
+        else cont (tailRecFactorialTR (x - 1) (fun(y) -> x * y)) in 
+                   tailRecFactorialTR x (fun(y) -> y)
+accFactorialWithContinuation 10
+
+// Using Sequences (sequence elements are generated dinamically whereas lists are stored entirely in memory)
+// Sequences help avoid StackOverflow
+let seqOfNumbers = seq {1 .. 5}
+seqOfNumbers |> Seq.iter (printfn "%d")
+
+//seq<int> = seq [1; 2; 3; 4;]
+let a = seq [1; 2; 3; 4;]
+let allPositiveIntsList = [for i in 1 .. System.Int32.MaxValue do yield i]      // this will fail with stackOverflow
+let allPositiveIntsSeq = seq {for i in 1 .. System.Int32.MaxValue do yield i}   // this will NOT fail with stack overflow
+
+// list of key value pairs
+let comingEvents = 
+    [
+    ("Palmesondag", "march 28");
+    ("Skaertorsdag", "april 1");
+    ("Langfredag", "april 2");
+    ("Paskedag", "april 4");
+    ]
+    |> Map.ofList
+//comingEvents.TryGetValue "Palmesondag"
+//comingEvents.Item "Palmesondag"
+comingEvents.["Palmesondag"]    // QUICK ACCESS of values from key value pairs
+
