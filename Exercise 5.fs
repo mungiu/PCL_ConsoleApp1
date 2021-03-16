4e3cdebcd89b75b5bb2504ca99864a28963c795b
Andrei Mungiu <andreimungiu@hotmail.com>
2021-03-16 19:38:03 +0100

    PCL HW WIP

--- Exercise 5.fs
+++ Exercise 5.fs
@@ -1,3 +1,29 @@
 ﻿module Exercise_5
 
-// Redefine the pclArea function (defined in Exercise 6.1) to use the new data type (PclShapeR). Call the new function pclAreaR
+// Redefine the pclArea function (defined in Exercise 6.1) to use the new data type (PclShapeR).
+// Call the new function pclAreaR
+
+// convert PclShape to use records
+type Shapes = RightTriangle | Rectangle
+type ShapeRecords = {length: float; width: float; shape: Shapes}
+let rectangle = {length = 2.0; width = 2.0; shape = Rectangle}
+let rightTriangle = {length = 2.0; width = 2.0; shape = RightTriangle}
+
+let areaR shape = 
+    match shape with
+    |{  ShapeRecords.length = length;
+        ShapeRecords.width = width;
+        ShapeRecords.shape = RightTriangle} -> 0.5 * length * width
+    |{  ShapeRecords.length = length;
+        ShapeRecords.width = width;
+        ShapeRecords.shape = Rectangle} -> length * width
+rectangle |> areaR
+
+let perimeterR shape = 
+    match shape with
+    |{  ShapeRecords.length = length;
+        ShapeRecords.width = width;
+        ShapeRecords.shape = Rectangle}  -> 2.0 * (length + width)
+    |{  ShapeRecords.length = length;
+        ShapeRecords.width = width;
+        ShapeRecords.shape = RightTriangle} -> length + width + sqrt(length**2.0 + width**2.0)
--- Exercise4.fs
+++ Exercise4.fs
@@ -1,7 +1,7 @@
 ﻿module Exercise4
 
-//type PclRightTrianle = int * int
-//type PclRectangle = float * float * 0.5
+//type PclRightTriangle = float * float
+//type PclRectangle = float * float 
 type PclShape = RightTriangle of float * float | Rectangle of float * float
 //let pclShape = PclRightTriangle (1.0, 1.0)
 //let pclShape' = PclRectangle (0.5, 1.0, 1.0)
@@ -17,6 +17,7 @@ let area shape =
 let perimeter shape = 
     match shape with
     | Rectangle (length, width) -> 2.0 * (length + width)
+    | RightTriangle (length, width) -> length + width + sqrt(length**2.0 + width**2.0)
 
 area rectangle
 area rightTriangle
@@ -24,7 +25,8 @@ perimeter rectangle
 
 
 // convert PclShape to use records
-type PclShapeR = {a: float; b: float}
+type Shapes = RightTriangle | Rectangle
+type ShapeRecords = {length: float; width: float; shape: Shapes}
 
 // MINI PROJECT - Drink To Go Online Cafe
 // decide which drinks you want to handle
