9d3873141b6c334f791ce9bcf31289ce608dba07
Andrei Mungiu <andreimungiu@hotmail.com>
2021-03-17 22:43:20 +0100

    Simple practice

--- Exercise 5.fs
+++ Exercise 5.fs
@@ -6,6 +6,7 @@
 // convert PclShape to use records
 type Shapes = RightTriangle | Rectangle
 type ShapeRecords = {length: float; width: float; shape: Shapes}
+
 let rectangle = {length = 2.0; width = 2.0; shape = Rectangle}
 let rightTriangle = {length = 2.0; width = 2.0; shape = RightTriangle}
 
@@ -27,3 +28,26 @@ let perimeterR shape =
     |{  ShapeRecords.length = length;
         ShapeRecords.width = width;
         ShapeRecords.shape = RightTriangle} -> length + width + sqrt(length**2.0 + width**2.0)
+
+
+
+///////// ALTERNATIVE SOLUTION 
+// The benefits here is that shapes are first defined and then 
+// a record of these shapes is created.
+// This allows much more flexibility for the shape records
+type RectangleR = {width : float; length: float}
+type RightTriangleR = {tbase : float; length : float}
+type EquilateralTriangle = {side : float}
+type GenericTriangle = {tbase : float; side2 : float; side3 : float; height: float}
+type ShapeRecord = 
+    | RR of RectangleR 
+    | RTR of RightTriangleR 
+    | EQTR of EquilateralTriangle 
+    | GT of GenericTriangle
+
+let pclAreaRecord shape =
+    match shape with
+    | RR {width = width; length = length} -> length * width
+    | RTR {tbase = tbase; length = length} -> 0.5 * tbase * length
+    | EQTR {side = side} -> ((sqrt 3.0)/4.0)*(side**2.0)
+    | GT {tbase = tbase; height = height} -> tbase * height / 2.0
