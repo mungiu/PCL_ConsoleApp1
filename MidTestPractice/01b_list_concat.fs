5a984f04d6cd065e06b8dae383ba7c8e8c0f60e0
Andrei Mungiu <andrei_mungiu@yahoo.com>
2021-05-06 14:09:19 +0200

    Exam Prep

---
+++ MidTestPractice/01b_list_concat.fs
@@ -0,0 +1,23 @@
+﻿module _01bExercise
+
+
+let a = 1 :: [2; 5; 3; 7]
+printf "%A" a
+// val it : int list = [1; 2; 5; 3; 7]
+
+let b = List.length [2; 5; 3; 7];;
+printf "%A" b
+//val it : int = 4
+
+//// NOTE: Only a list can be added at the end of a list
+//let c = [2; 5; 3; 7] :: 4;;
+
+let rec addToEnd list number =
+    match list with
+    | head :: tail when tail <> [] -> head :: addToEnd tail number
+    | head :: tail when tail = [] -> head :: number
+    | _ -> list
+printf "%A" (addToEnd [2; 5; 3; 7] [4; 8])
+printf "%A" (addToEnd [2; 5; 3; 7] [4])
+//val it : int list = [2; 5; 3; 7; 4; 8]
+//val it : int list = [2; 5; 3; 7; 4]
