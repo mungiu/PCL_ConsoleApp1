cb6d79ef086bbde9da94fe1a46c417e49601df04
Andrei Mungiu <andrei_mungiu@yahoo.com>
2021-05-07 16:21:28 +0200

    Exam prep

---
+++ MidTestPractice/02a_strings.fs
@@ -0,0 +1,22 @@
+﻿module _02a_match
+
+let vowelToUpper (char:char) =
+    match char with
+    | 'a' ->'A'
+    | 'e'->'E'
+    | 'i'->'I'
+    | 'o'->'O'
+    | 'u'-> 'U'
+    | _ -> char
+printfn "%A" (vowelToUpper 'a')
+printfn "%A" (vowelToUpper 'x')
+//val vowelToUpper : char:string -> string
+//"A"val it : unit = ()
+//"x"val it : unit = ()
+
+
+let rec vowelToUpperStrRec (str:string) =
+    match str with
+    | "" -> ""
+    | str -> (vowelToUpper str.[0]).ToString() + vowelToUpperStrRec str.[1..(String.length(str) - 1)]
+vowelToUpperStrRec "Andrei Mungiu"
---
+++ MidTestPractice/02b_rec_list.fs
@@ -0,0 +1,19 @@
+﻿module _2b
+
+let rec pmLength list =
+    match list with
+    | hd::tl when tl <> [] -> 1 + pmLength tl
+    | _ -> 1
+pmLength ['x'; 'y'; 'z' ]
+
+let rec pclSum list =
+    match list with
+    | hd::tl when tl <> [] -> hd + pclSum tl
+    | hd::tl when tl = [] -> hd
+pclSum [2; 3; 5; 8 ]
+
+let rec takeSome qty list =
+    match list with
+    | hd::tl when qty > 0 -> hd :: takeSome (qty-1) tl
+    | hd::tl when qty = 0 -> []
+takeSome 2 ['a'; 'b'; 'c'; 'd'] 
